cc3e5d22f757b15803bf744ad416485fbe2695e6
llimaarthur <thearthurtm@gmail.com>
2026-08-21 17:06:43 -0300

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -9,5 +9,5 @@ salario_total = salario_fixo + comissao
 print(f"Comissão: R$ {comissao}")
 print(f"Salário total: R$ {salario_total}")
 
-# Testes realizados conforme o documento
+# Testes realizados conforme o documento!
 # Valores utilizados: R$1.500,00, R$ 2.000,00 E R$ 2.500,00.
