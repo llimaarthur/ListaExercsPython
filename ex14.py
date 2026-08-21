8e660fb6e899b561183c6ec2cf6a7720ccaff6f7
llimaarthur <thearthurtm@gmail.com>
2026-08-21 16:58:51 -0300

    Add files via upload

---
+++ ex14.py
@@ -0,0 +1,11 @@
+# Exercício 14 - Troca de valores.
+
+a = int(input("A: "))
+b = int(input("B: "))
+
+print("Depois da troca: ")
+print(f"A: {b}")
+print(f"B: {a}")
+
+# Testes realizados conforme o documento
+# Valores utilizados: 1 e 2, -5 e 10 e 7 e 7.
