e0fa77b38636ce2e2f29d5a693a3081523e3b5b4
llimaarthur <thearthurtm@gmail.com>
2026-08-21 17:06:59 -0300

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -7,5 +7,5 @@ print("Depois da troca: ")
 print(f"A: {b}")
 print(f"B: {a}")
 
-# Testes realizados conforme o documento
+# Testes realizados conforme o documento!
 # Valores utilizados: 1 e 2, -5 e 10 e 7 e 7.
