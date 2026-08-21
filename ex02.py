9a5e2570bf023d8006e8478131b224f27817d195
llimaarthur <thearthurtm@gmail.com>
2026-08-21 00:43:19 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,7 +1,7 @@
 # Exercício 2 - Média de duas notas.
 
-nota1 = float (input("Nota 1: "))
-nota2 = float (input("Nota 2: "))
+nota1 = float(input("Nota 1: "))
+nota2 = float(input("Nota 2: "))
 
 media = (nota1 + nota2) / 2
 
