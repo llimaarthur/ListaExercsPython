4863e777fc93398da875d148963fe1483b6faef6
llimaarthur <thearthurtm@gmail.com>
2026-08-25 20:18:16 -0300

    Add files via upload

---
+++ 1 ao 15/ex01.py
@@ -0,0 +1,11 @@
+# Exercício 1 - Soma de 2 números.
+
+a = float(input("Primeiro valor: "))
+b = float(input("Segundo valor: "))
+
+soma = a + b
+
+print(f"Soma: {soma}")
+
+# Testes realizados conforme o documento!
+# Números usados: 4 e 9, -3 e 8 e 0 e 12.
---
+++ 1 ao 15/ex09.py
@@ -0,0 +1,13 @@
+# Exercicío 9 - Reajuste salarial.
+
+salario_atual = float(input("Salário atual: R$ "))
+
+aumento =  salario_atual * 0.15
+
+novo_salario = salario_atual + aumento
+
+print(f"Aumento: R$ {aumento}")
+print(f"Novo salário: R$ {novo_salario}")
+
+# Testes realizados conforme o documento!
+# Valores utilizados: R$ 1.200,00, 3.500,00 e R$ 800,00.
